8625efe5b16077a5e9c8a35b3bcc9d50a5778a3e
Shane Guittap <shaneguittap@Kristins-MacBook-Pro-2.local>
2018-08-29 13:27:24 -0600

    "created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,23 @@
 var cards = ["queen", "queen", "king", "king"];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped" + " " + cardOne);
-var cardTwo = cards[3];
-cardsInPlay.push('cardTwo');
-console.log("User flipped" + " " + cardTwo);
 
+var checkForMatch = function (){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){ 
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
+	}	
+};
 
-if (cardsInPlay.length === 2);
+var flipCard = function (cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch ();
+};
 
-	else if (cardsInPlay[0] === cardsInPlay[1]) 
-		{alert ("You found a match!");
-}
-	else (cardsInPlay[0] !== cardsInPlay[1])
-		{alert ("Sorry, try again.");
-}
-
+flipCard(0);
+flipCard(2);
 
